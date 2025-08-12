5900e30adedcf96db5765cb233351c65c2986662
Macko <UI935414@rwe-ag.com>
2025-08-12 16:17:59 +0200

    Initial MCP setup

---
+++ Tools/ReportingTools.cs
@@ -0,0 +1,11 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace ReportingMCP.Tools;
+
+[McpServerToolType]
+public static class ReportingTools
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => "Hello from Reporting MCP";
+}
